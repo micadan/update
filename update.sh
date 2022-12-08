d1afb47d47aab21a0477f86d1d224800ce6d4ff9
micadan <120133934+micadan@users.noreply.github.com>
2022-12-08 21:34:26 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# bash <(curl -s https://raw.githubusercontent.com/micadan/nodes/main/update/update.sh)
+
+cd $HOME/pathfinder
+docker-compose down
+docker-compose pull
+docker-compose up -d
+
+docker-compose -f $HOME/pathfinder/docker-compose.yml logs -f --tail=10
